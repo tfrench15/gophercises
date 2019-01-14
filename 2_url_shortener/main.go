657850d14f9be240104f745968cb409aae102084
Tim French <tfrench15@gmail.com>
2019-01-14 15:56:31 -0800

    clean up main

--- 2_url_shortener/main.go
+++ 2_url_shortener/main.go
@@ -8,29 +8,6 @@ import (
 )
 
 func main() {
-	yml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-	_, err := urlshort.YAMLHandler([]byte(yml), nil)
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-
-func defaultMux() *http.ServeMux {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", hello)
-	return mux
-}
-
-func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, world!")
-}
-
-func placeholder() {
 	mux := defaultMux()
 
 	// Build the MapHandler using the mux as the fallback
@@ -42,16 +19,25 @@ func placeholder() {
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
 	yml := `
-	- path: /urlshort
-	  url: https://github.com/gophercises/urlshort
-	- path: /urlshort-final
-	  url: http://github.com/gophercises/urlshort/tree/solution`
-
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/solution
+`
 	ymlHandler, err := urlshort.YAMLHandler([]byte(yml), mapHandler)
 	if err != nil {
 		panic(err)
 	}
-
 	fmt.Println("Starting the server on :8000")
 	http.ListenAndServe(":8000", ymlHandler)
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
 }
