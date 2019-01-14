087e2ccf7702f1847957ca67402b8656b08589aa
Tim French <tfrench15@gmail.com>
2019-01-14 13:36:36 -0800

    first commit

---
+++ 2_url_shortener/urlshort/handler.go
@@ -0,0 +1,12 @@
+package urlshort
+
+import "net/http"
+
+// MapHandler will return an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return nil
+}
