7e6879ee78a0f400d2f51e793504f04d15945ce1
Tim French <tfrench15@gmail.com>
2019-01-15 21:56:52 -0800

    finish part 1 of exercise 1

--- 1_quiz_game/main.go
+++ 1_quiz_game/main.go
@@ -1,9 +1,66 @@
 package main
 
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
 func main() {
-	// TODO: run the game...
+	f, err := openFile()
+	if err != nil {
+		panic(err)
+	}
+	data, err := readFile(f)
+	if err != nil {
+		panic(err)
+	}
+	quiz := createProblems(data)
+	playGame(quiz)
 }
 
-func openFile() {
-	// TODO: open the file...
+type problem struct {
+	question string
+	answer   string
+}
+
+func openFile() (*os.File, error) {
+	return os.Open("problems.csv")
+}
+
+func readFile(file *os.File) ([][]string, error) {
+	r := csv.NewReader(file)
+	problems, err := r.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+	return problems, nil
+}
+
+func createProblems(problems [][]string) []problem {
+	quiz := []problem{}
+	for _, item := range problems {
+		p := problem{
+			question: item[0],
+			answer:   item[1],
+		}
+		quiz = append(quiz, p)
+	}
+	return quiz
+}
+
+func playGame(quiz []problem) {
+	correct := 0
+	scanner := bufio.NewScanner(os.Stdin)
+	for i := 0; i < len(quiz); i++ {
+		fmt.Printf("Question: %s ", quiz[i].question)
+		scanner.Scan()
+		ans := scanner.Text()
+		if ans == quiz[i].answer {
+			correct++
+		}
+		continue
+	}
+	fmt.Printf("End of Quiz. Scored %d out of %d\n", correct, len(quiz))
 }
