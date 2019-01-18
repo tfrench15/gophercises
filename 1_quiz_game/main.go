e3bcfebb2b54224a7dad0269aaa42bbfc469b576
Tim French <tfrench15@gmail.com>
2019-01-18 15:54:55 -0800

    finish part II

--- 1_quiz_game/main.go
+++ 1_quiz_game/main.go
@@ -3,24 +3,61 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
+var timeLimit = flag.Int("limit", 30, "sets the time limit for the quiz")
+
 func main() {
+	flag.Parse()
 	f, err := openFile()
 	check(err)
 
 	data, err := readFile(f)
 	check(err)
 
-	quiz := createProblems(data)
-	playGame(quiz)
+	quiz := newQuiz(data)
+	tl := *timeLimit
+	limit := time.Duration(tl) * time.Second
+	quiz.play(limit)
+}
+
+type quiz struct {
+	problems []problem
+	score    int
 }
 
 type problem struct {
 	question string
 	answer   string
+}
+
+func (q *quiz) play(limit time.Duration) {
+	scanner := bufio.NewScanner(os.Stdin)
+	ch := make(chan int)
+	go func() {
+		for i := 0; i < len(q.problems); i++ {
+			fmt.Printf("Question %d: %s ", i+1, q.problems[i].question)
+			scanner.Scan()
+			ans := scanner.Text()
+			if ans == q.problems[i].answer {
+				q.score++
+			}
+			continue
+		}
+		ch <- 1
+	}()
+	select {
+	case <-ch:
+		fmt.Printf("Quiz Complete: Scored %d out %d\n", q.score, len(q.problems))
+		return
+	case <-time.After(limit):
+		fmt.Printf("Quiz Complete: Scored %d out %d\n", q.score, len(q.problems))
+		return
+	}
 }
 
 func openFile() (*os.File, error) {
@@ -36,33 +73,23 @@ func readFile(file *os.File) ([][]string, error) {
 	return problems, nil
 }
 
-func createProblems(problems [][]string) []problem {
-	quiz := []problem{}
+func newQuiz(problems [][]string) quiz {
+	list := []problem{}
 	for _, item := range problems {
 		p := problem{
 			question: item[0],
 			answer:   item[1],
 		}
-		quiz = append(quiz, p)
+		list = append(list, p)
 	}
-	return quiz
+	q := quiz{
+		problems: list,
+		score:    0,
+	}
+	return q
 }
 
-func playGame(quiz []problem) {
-	correct := 0
-	scanner := bufio.NewScanner(os.Stdin)
-	for i := 0; i < len(quiz); i++ {
-		fmt.Printf("Question: %s ", quiz[i].question)
-		scanner.Scan()
-		ans := scanner.Text()
-		if ans == quiz[i].answer {
-			correct++
-		}
-		continue
-	}
-	fmt.Printf("End of Quiz. Scored %d out of %d\n", correct, len(quiz))
-}
-
+// handle errs.
 func check(err error) {
 	if err != nil {
 		panic(err)
